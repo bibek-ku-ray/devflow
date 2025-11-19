1d6c9195bef413bffc1a2800ae95824fc4b87f12
Bibek Ray <bibek.ray@deerwalk.edu.np>
2025-11-19 20:59:07 +0545

    Add new logo assets in SVG and PNG formats
    - Added logo-light.svg for the light-themed logo.
    - Added logo.png for the primary logo in PNG format.
    - Added site-logo.svg for the site logo in SVG format.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   console.log("What am i")
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black font-black text-2xl">
-      <h1>Good evening🦹, Bibek!</h1>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-900 font-sans dark:bg-black font-black text-2xl">
+      <h1 className="text-primary-500 font-space-grotesk text-9xl"><span className="text-white">Good evening,</span> Bibek!</h1>
     </div>
   );
 }
